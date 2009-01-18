13afefca69c598a3df28f2a6cf5528dd5c3b6bc5
cash <cash.costello@gmail.com>
2009-01-18 22:41:10 +0000

    added edit strings

--- languages/en.php
+++ languages/en.php
@@ -3,7 +3,11 @@
 	$english = array(	
       'simplepie:widget' => 'Blog',
       'simplepie:description' => 'Add an external blog to your profile',
-      'simplepie:notset' => 'Feed url is not set',	
+      'simplepie:notset' => 'Feed url is not set',
+      'simplepie:feed_url' => 'Feed URL',
+      'simplepie:num_items' => 'Number of items',
+      'simplepie:excerpt' => 'Include excerpt',	
+      'simplepie:post_date' => 'Include post date',	
 	);
 					
 	add_translation("en",$english);
