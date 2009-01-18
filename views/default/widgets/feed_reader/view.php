33c172943ddd2431773bbdb3c4cd23fce295f8d5
cash <cash.costello@gmail.com>
2009-01-18 22:32:30 +0000

    added settings on num of entries to include and whether to include the post date

--- views/default/widgets/feed_reader/view.php
+++ views/default/widgets/feed_reader/view.php
@@ -9,29 +9,44 @@
   $feed_url = $vars['entity']->feed_url;
   if($feed_url){
 
-    $feed = new SimplePie($feed_url);
+    $excerpt   = $vars['entity']->excerpt;
+    $num_items = $vars['entity']->num_items;
+    $post_date = $vars['entity']->post_date;
+     
+    echo $feed_url;
+    $feed = new SimplePie();
+    $feed->set_feed_url($feed_url);
+    //$feed->set_autodiscovery_level(SIMPLEPIE_LOCATOR_ALL);
+    $feed->init();
     $feed->handle_content_type();
-
-
+echo $post_date;
+  echo $feed->get_item_quantity();
 ?>
-<h1><a href="<?php echo $feed->get_permalink(); ?>"><?php echo $feed->get_title(); ?></a></h1>
-	<?php
-	/*
-	Here, we'll loop through all of the items in the feed, and $item represents the current item in the loop.
-	*/
-	foreach ($feed->get_items() as $item):
-	?>
+<h1><a href="<?php echo $feed->get_permalink(); ?>"><?php echo $feed->get_title(); ?></a></h1><br />
+<?php
+  if ($num_items > $feed->get_item_quantity())
+    $num_items = $feed->get_item_quantity();
+    
+  foreach ($feed->get_items(0,$num_items) as $item):
+?>
  
 		<div class="item">
-			<h2><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></h2>
-			<p><?php echo $item->get_description(); ?></p>
-			<p><small>Posted on <?php echo $item->get_date('j F Y | g:i a'); ?></small></p>
+			<p><h3><a href="<?php echo $item->get_permalink(); ?>"><?php echo $item->get_title(); ?></a></h3></p>
+			<?php if ($excerpt) echo '<p>' . $item->get_description(true) . '</p>'; ?>
+      <?php if ($post_date) 
+            {
+      ?>
+              <p><small>Posted on <?php echo $item->get_date('j F Y | g:i a'); ?></small></p>
+      <?php } ?>
 		</div>
  
 	<?php endforeach; ?>
+
 <?php 
   } else {
         
     echo '<p>' . elgg_echo('simplepie:notset') . '</p>';      
   }
 ?>
+
+<!--			<p><?php //echo $item->get_description(); ?></p> -->
