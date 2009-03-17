1f5aabe7bd30eb0035c862ce455ec2a5ee9e6404
cash <cash.costello@gmail.com>
2009-03-17 11:46:37 +0000

    added error string

--- languages/en.php
+++ languages/en.php
@@ -4,6 +4,7 @@
       'simplepie:widget' => 'Blog',
       'simplepie:description' => 'Add an external blog to your profile',
       'simplepie:notset' => 'Feed url is not set',
+      'simplepie:notfind' => 'Cannot find feed. Check the feed url.',
       'simplepie:feed_url' => 'Feed URL',
       'simplepie:num_items' => 'Number of items',
       'simplepie:excerpt' => 'Include excerpt',	
