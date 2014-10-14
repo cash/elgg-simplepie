68ec6e821e7cd33d599d3392747de49e7d316978
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2014-10-14 10:44:16 +0300

    Adds finnish translation

---
+++ languages/fi.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'simplepie:widget' => 'RSS-syöte',
+	'simplepie:description' => 'Lisää ulkoinen syöte profiilisivullesi.',
+	'simplepie:notset' => 'Syötteen osoite puuttuu.',
+	'simplepie:notfind' => 'Syötettä ei löydy. Tarkista syötteen osoite.',
+	'simplepie:feed_url' => 'Syötteen URL',
+	'simplepie:num_items' => 'Näytettävien kohteiden määrä',
+	'simplepie:excerpt' => 'Näytä tiivistelmä',
+	'simplepie:post_date' => 'Näytä päivämäärä',
+	'simplepie:postedon' => 'Kirjoitettu',
+);
